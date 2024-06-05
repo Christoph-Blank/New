ee792f6a2edf2ec1e505e9f459dad9bcf5d2c9b3
Christoph-Blank <91676740+Christoph-Blank@users.noreply.github.com>
2024-06-05 15:25:05 +0200

    Update config.js

--- Basic/config.js
+++ Basic/config.js
@@ -4,11 +4,11 @@ const Config = {
         fetchBroadcastUrl: "./resources/newsFeedPortalAlerts.json"
     },
     namedProjections: [
-       // ETRS89 UTM  -- 25833 hinzugfuegt
-      //["EPSG:25832", "+title=ETRS89/UTM 32N +proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"],
+       // ETRS89 UTM  -- 25833 hinzugfuegt      
       ["EPSG:25833", "+title=ETRS89/UTM 33N +proj=utm +zone=33 +ellps=WGS84 +towgs84=0,0,0,0,0,0,1 +units=m +no_defs"],
       // Soldner Berlin --hinzugefuegt
-      ["EPSG:3068", "+title=Soldner Berlin +proj=cass +lat_0=52.41864827777778 +lon_0=13.62720366666667 +x_0=40000 +y_0=10000 +ellps=bessel +datum=potsdam +units=m +no_defs"],     
+      ["EPSG:3068", "+title=Soldner Berlin +proj=cass +lat_0=52.41864827777778 +lon_0=13.62720366666667 +x_0=40000 +y_0=10000 +ellps=bessel +datum=potsdam +units=m +no_defs"],
+      ["EPSG:4326", "+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"],
     ],
     layerConf: "./resources/services-internet.json",
     restConf: "./resources/rest-services-internet.json",
